a4f37c517ef372dd7c410dfa0595a155e1f20bfb
nylee9621 <nylee9621@gmail.com>
2022-01-04 02:15:00 +0900

    Modify: 프로그래머스 문자열 내 마음대로 정렬하기

--- programmers/sortStrOnMyWay.js
+++ programmers/sortStrOnMyWay.js
@@ -4,8 +4,8 @@ const n = 2;
 function solution(strings, n) {
   return strings.sort((a, b) => {
     if(a[n] < b[n]) return -1;
-    else if(a[n] > b[n]) return 1;
-    else return a - b;
+    else if(a[n] === b[n] && a < b) return -1;
+    else return 1;
   });
 }
 
