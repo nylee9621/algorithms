aeee28d35d2a742b8274d0329e4f433968aa0574
nylee9621 <nylee9621@gmail.com>
2022-03-01 23:56:23 +0900

    Modify: 프로그래머스 숫자 문자열과 영단어

--- programmers/numStr&alphabet.js
+++ programmers/numStr&alphabet.js
@@ -1,17 +1,18 @@
 const s = "one4seveneight";
 
 function solution(s) {
-    const nums = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'};
-    let result = s;
+  const nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let result = s;
+  let i = 0;
 
-    for(let i = 0; i < Object.keys(nums); i++) {
-        const idx = s.indexOf(Object.keys(nums)[i]);
-        while(idx > -1) {
-            s = s.slice(0, idx) + nums[idx] + s.slice(idx + Object.keys(nums).length);
-        }
-    }
-
-    return Number(result);
+  while(true) {
+    if(i === nums.length) break;
+    const idx = result.indexOf(nums[i]);
+    if(idx > -1) {
+    	result = result.slice(0, idx) + i + result.slice(idx + nums[i].length);
+    } else i++;
+  }
+  return Number(result);
 }
 
 solution(s);
