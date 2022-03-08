09f06d6f217536201660c73190e6df4ceef4e13f
nylee9621 <nylee9621@gmail.com>
2022-03-08 23:50:04 +0900

    Modify: 프로그래머스 피로도

--- programmers/fatigueLevel.js
+++ programmers/fatigueLevel.js
@@ -2,7 +2,9 @@ const k = 80;
 const dungeons = [[80,20],[50,40],[30,10]];
 
 function solution(k, dungeons) {
+  let result = 0;
   dungeons.sort((a, b) => a[0] - b[0]);
+  return result;
 }
 
 solution(k, dungeons);
