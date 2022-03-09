80bcc7dac44db76fa368d3da52c4dff3a52d6683
nylee9621 <nylee9621@gmail.com>
2022-03-09 23:57:21 +0900

    Modify: 프로그래머스 피로도

--- programmers/fatigueLevel.js
+++ programmers/fatigueLevel.js
@@ -1,10 +1,36 @@
 const k = 80;
 const dungeons = [[80,20],[50,40],[30,10]];
 
+function permutation(arr, num){
+  const res = [];
+  if(num === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const rest = [...arr.slice(0,idx), ...arr.slice(idx+1)];
+    const permutations = permutation(rest, num-1);
+    const attach = permutations.map((permutation) => [v, ...permutation]);
+    res.push(...attach);
+  })
+  return res;
+}
+
 function solution(k, dungeons) {
-  let result = 0;
-  dungeons.sort((a, b) => a[0] - b[0]);
-  return result;
+  const per = permutation(dungeons, dungeons.length);
+  let max = 0;
+	console.log(per);
+  for(let i = 0; i < per.length; i++) {
+    let temp = k;
+    for(let j = 0; j < per[i].length; j++) {
+      if(temp >= per[i][j][0]) {
+        temp -= per[i][j][1];
+      } else {
+        if(j > max) max = j;
+        break;
+      }
+      console.log(temp);
+    }
+  }
+  // return max;
 }
 
 solution(k, dungeons);
