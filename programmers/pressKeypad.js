898d220e0a8566d63f7fbae3c7cf18567d53f8f8
nylee9621 <nylee9621@gmail.com>
2022-03-12 22:56:31 +0900

    Modify: 프로그래머스 키패드 누르기

--- programmers/pressKeypad.js
+++ programmers/pressKeypad.js
@@ -2,7 +2,8 @@ const numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
 const hand = 'right';
 
 function solution(numbers, hand) {
-
+    let result = 0;
+    return result;
 }
 
 solution(numbers, hand);
