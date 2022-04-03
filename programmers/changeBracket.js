6d78e35d3a5de0fb73f56bc61bf28910c6d7dddc
nylee9621 <nylee9621@gmail.com>
2022-04-03 23:17:41 +0900

    Modify: 프로그래머스 괄호 변환

--- programmers/changeBracket.js
+++ programmers/changeBracket.js
@@ -4,30 +4,16 @@ function solution(p) {
   let result = '';
   let open = 0;
   let close = 0;
-  let stack = [];
+  let check = false;
 
   if(p === '') return p;
 
-  while(true) {
-    if(true) {
-
-    } else {
-
-    }
+  for(let i = 0; i < p.length; i++) {
+    if (p[i] == '(') left++;
+		if (p[i] == ')') right++;
   }
+
   return result;
-}
-
-function check(p) {
-  var stack = [];
-  for (let i = 0; i < p.length; i++) {
-      if (p[i] === '(') stack.push('(')
-      else {
-          if (stack.length === 0) return false;
-          stack.pop();
-      }
-  }
-  return true;
 }
 
 solution(p);
