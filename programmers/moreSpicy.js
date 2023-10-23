b5abcdd66e22f67acf40779cf2848fb31647d182
nylee9621 <nylee9621@gmail.com>
2023-10-23 23:35:21 +0900

    Modify: 프로그래머스 더 맵게

--- programmers/moreSpicy.js
+++ programmers/moreSpicy.js
@@ -75,3 +75,70 @@ function solution(scoville, K) {
 }
 
 solution(scoville, K);
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  swap(idx1, idx2) {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  }
+
+  add(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  poll() {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    const value = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return value;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    let parentIdx = Math.floor((index - 1) / 2);
+    while (
+      this.heap[parentIdx] &&
+      this.heap[index][1] < this.heap[parentIdx][1]
+    ) {
+      this.swap(index, parentIdx);
+      index = parentIdx;
+      parentIdx = Math.floor((index - 1) / 2);
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+    let leftIdx = index * 2 + 1;
+    let rightIdx = index * 2 + 2;
+
+    while (
+      (this.heap[leftIdx] && this.heap[leftIdx][1] < this.heap[index][1]) ||
+      (this.heap[rightIdx] && this.heap[rightIdx][1] < this.heap[index][1])
+    ) {
+      let smallerIdx = leftIdx;
+      if (
+        this.heap[rightIdx] &&
+        this.heap[rightIdx][1] < this.heap[smallerIdx][1]
+      ) {
+        smallerIdx = rightIdx;
+      }
+
+      this.swap(index, smallerIdx);
+      index = smallerIdx;
+      leftIdx = index * 2 + 1;
+      rightIdx = index * 2 + 2;
+    }
+  }
+}
